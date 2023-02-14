5a4ef69e80cd002c7b4207e8545ba80b364ec89c
2278228941 <2278228941@qq.com>
2023-02-14 23:47:01 +0800

    新增一个一级路由首页

---
+++ src/view/BspIndex.vue
@@ -0,0 +1,32 @@
+<template>
+<div class=''></div>
+</template>
+
+<script>
+export default {
+data() {
+//这里存放数据
+return {
+
+};
+},
+//计算属性
+computed: {},
+//方法集合
+methods: {
+
+},
+//挂载完成（可以访问DOM元素）
+mounted() {
+
+},
+//监控data中的数据变化
+watch: {
+
+}
+}
+</script>
+<style  scoped>
+/*@import url(); 引入公共css类*/
+
+</style>
